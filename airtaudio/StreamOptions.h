a5dbe5a6072732f0a09abc233d7a2eea4c038287
Edouard DUPIN <yui.heero@gmail.com>
2015-02-26 23:49:26 +0100

    [DEV] start add option

--- airtaudio/StreamOptions.h
+++ airtaudio/StreamOptions.h
@@ -9,15 +9,23 @@
 #define __AIRTAUDIO_STREAM_OPTION_H__
 
 namespace airtaudio {
+
+	enum timestampMode {
+		timestampMode_Hardware, //!< enable harware timestamp
+		timestampMode_trigered, //!< get harware triger time stamp and ingrement with duration
+		timestampMode_soft, //!< Simulate all timestamp.
+	};
 	class StreamOptions {
 		public:
 			airtaudio::Flags flags; //!< A bit-mask of stream flags
 			uint32_t numberOfBuffers; //!< Number of stream buffers.
 			std::string streamName; //!< A stream name (currently used only in Jack).
+			enum timestampMode mode; //!< mode of timestamping data...
 			// Default constructor.
 			StreamOptions() :
 			  flags(),
-			  numberOfBuffers(0){}
+			  numberOfBuffers(0),
+			  mode(timestampMode_Hardware) {}
 	};
 };
 
