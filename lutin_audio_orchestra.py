5992923c887b5162ccecb9e380b6aaa18db896b2
Edouard DUPIN <yui.heero@gmail.com>
2015-05-08 22:35:39 +0200

    [BUILD] update new lutin 0.5.3

--- lutin_audio_orchestra.py
+++ lutin_audio_orchestra.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-import lutinModule as module
-import lutinTools as tools
-import lutinDebug as debug
+import lutin.module as module
+import lutin.tools as tools
+import lutin.debug as debug
 
 def get_desc():
 	return "audio_orchestra : Generic wrapper on all audio interface"
@@ -27,7 +27,7 @@ def create(target):
 		])
 	myModule.add_module_depend(['audio', 'etk'])
 	# add all the time the dummy interface
-	myModule.add_export_flag_CC(['-DORCHESTRA_BUILD_DUMMY'])
+	myModule.add_export_flag('c++', ['-DORCHESTRA_BUILD_DUMMY'])
 	# TODO : Add a FILE interface:
 	
 	if target.name=="Windows":
@@ -36,9 +36,9 @@ def create(target):
 			'audio/orchestra/api/Ds.cpp',
 			])
 		# load optionnal API:
-		myModule.add_optionnal_module_depend('asio', "ORCHESTRA_BUILD_ASIO")
-		myModule.add_optionnal_module_depend('ds', "ORCHESTRA_BUILD_DS")
-		myModule.add_optionnal_module_depend('wasapi', "ORCHESTRA_BUILD_WASAPI")
+		myModule.add_optionnal_module_depend('asio', ["c++", "-DORCHESTRA_BUILD_ASIO"])
+		myModule.add_optionnal_module_depend('ds', ["c++", "-DORCHESTRA_BUILD_DS"])
+		myModule.add_optionnal_module_depend('wasapi', ["c++", "-DORCHESTRA_BUILD_WASAPI"])
 	elif target.name=="Linux":
 		myModule.add_src_file([
 			'audio/orchestra/api/Alsa.cpp',
@@ -46,25 +46,25 @@ def create(target):
 			'audio/orchestra/api/Pulse.cpp',
 			'audio/orchestra/api/Oss.cpp'
 			])
-		myModule.add_optionnal_module_depend('alsa', "ORCHESTRA_BUILD_ALSA")
-		myModule.add_optionnal_module_depend('jack', "ORCHESTRA_BUILD_JACK")
-		myModule.add_optionnal_module_depend('pulse', "ORCHESTRA_BUILD_PULSE")
-		myModule.add_optionnal_module_depend('oss', "ORCHESTRA_BUILD_OSS")
+		myModule.add_optionnal_module_depend('alsa', ["c++", "-DORCHESTRA_BUILD_ALSA"])
+		myModule.add_optionnal_module_depend('jack', ["c++", "-DORCHESTRA_BUILD_JACK"])
+		myModule.add_optionnal_module_depend('pulse', ["c++", "-DORCHESTRA_BUILD_PULSE"])
+		myModule.add_optionnal_module_depend('oss', ["c++", "-DORCHESTRA_BUILD_OSS"])
 	elif target.name=="MacOs":
 		myModule.add_src_file([
 							   'audio/orchestra/api/Core.cpp',
 							   'audio/orchestra/api/Oss.cpp'
 							   ])
 		# MacOsX core
-		myModule.add_optionnal_module_depend('CoreAudio', "ORCHESTRA_BUILD_MACOSX_CORE")
+		myModule.add_optionnal_module_depend('CoreAudio', ["c++", "-DORCHESTRA_BUILD_MACOSX_CORE"])
 	elif target.name=="IOs":
 		myModule.add_src_file('audio/orchestra/api/CoreIos.mm')
 		# IOsX core
-		myModule.add_optionnal_module_depend('CoreAudio', "ORCHESTRA_BUILD_IOS_CORE")
+		myModule.add_optionnal_module_depend('CoreAudio', ["c++", "-DORCHESTRA_BUILD_IOS_CORE"])
 	elif target.name=="Android":
 		myModule.add_src_file('audio/orchestra/api/Android.cpp')
 		# specidic java interface for android:
-		myModule.add_optionnal_module_depend('ewolAndroidAudio', "ORCHESTRA_BUILD_JAVA")
+		myModule.add_optionnal_module_depend('ewolAndroidAudio', ["c++", "-DORCHESTRA_BUILD_JAVA"])
 		#myModule.add_module_depend(['ewol'])
 	else:
 		debug.warning("unknow target for audio_orchestra : " + target.name);
